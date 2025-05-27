19612ec0d69aef64b44cbc96b953cb9fdd47c192
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2025-05-27 23:14:17 +0300

    update

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_OpticalProject_FILE /Volumes/trancend/learn/it/C++/Polytechic/Lab/02-03/Optical-Device/cmake-build-debug/OpticalProject.app/Contents/MacOS/OpticalProject)
-set(__QT_DEPLOY_TARGET_OpticalProject_TYPE EXECUTABLE)
